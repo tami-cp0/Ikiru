e0aeb53a1cf44e970c2b401cb3f0e8f1873543ce
tami-cp0 <findtamilore>
2024-04-15 09:50:11 +0100

    Update: mysql setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,13 @@
 -- prepares a MySQL server for the project
 -- this is just a temporary setup
 -- i also hard coded the details when creating the engine
-DROP DATABASE IF EXISTS ikiru_dev_db;
 
+-- if they say the password does not meet password requirement, use this command to check your policy
+-- SHOW VARIABLES LIKE 'validate_password.policy';
+-- then add "SET GLOBAL validate_password.policy=LOW;" to line 12
+-- then add "SET GLOBAL validate_password.policy=<YOUR_POLICY>;" TO line 14
+
+DROP DATABASE IF EXISTS ikiru_dev_db;
 CREATE DATABASE IF NOT EXISTS ikiru_dev_db;
 
 CREATE USER IF NOT EXISTS 'ikiru_user'@'localhost' IDENTIFIED BY 'password';
