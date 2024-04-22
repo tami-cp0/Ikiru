934e6f8601dd87382bc8090d6c25054670f85459
tami-cp0 <findtamilore>
2024-04-22 13:25:14 +0100

    .

--- test.py
+++ test.py
@@ -8,3 +8,6 @@ from datetime import date
 dat = "2000-04-02"
 con = date.fromisoformat(dat)
 print(con)
+
+
+{"name": "Josh", "username": "ragoyam", "sex": "Male", "password": "as8asPju", "email": "joshal@gmail.com", "dob": "2010-04-17"}
