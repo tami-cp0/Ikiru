588aece6cf70966a423e02c07a2b06dfd76a63cb
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-04-29 11:55:16 -0700

    update: socketio errors

--- temp/ikiruIO/realtime.py
+++ temp/ikiruIO/realtime.py
@@ -1,4 +1,5 @@
 from flask_socketio import emit
+from flask import request
 from temp.ikiruIO import socket
 
 @socket.on("connect")
@@ -10,4 +11,4 @@ def handle_connect():
 def handle_sent_message(json_message):
     """direct the sent message to the reveciver"""
     namespace = "/msg"+ json_message["receiver"] + "/" + request.sid
-    emit("received_msg", json_message['msg'], namespace=namespace)
+    emit("received_msg", json_message, namespace="/msg")
