be19403b85573fc785a9e21a52d76bbdf157d952
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-05-02 13:12:42 -0700

    add: new.js file created

---
+++ web_app/static/scripts/new.js
@@ -0,0 +1,30 @@
+$(() => {
+    socket = io.connect("http://127.0.0.1:5000");
+    convoio = io.connect("http://127.0.0.1:5000/home/${username}");
+  
+  
+    convoio.on("private", () => {
+      convo_contianer = $("get the coversation selector")
+  
+    });
+  
+  // this listen to post user posted on "http://127.0.0.1:5000/home" socket
+    socket.on("post", () => {
+  
+    });
+  
+    //post using socket connected to http://127.0.0.1:5000/home
+    // everyone that his/her socket connected using http://127.0.0.1:5000/home and listening receive the post instantly
+    $().on("click" () => {
+      
+      // send the post to backend eventlistener "post"
+      socket.emit("post", data);
+    });
+  
+    inboxio = io.connect("http://127.0.0.1:5000/msg/${my_username}");
+    // send
+    $("#send").on("click", () => {
+  
+      socket.emit("sent_message", data);
+    });
+});
